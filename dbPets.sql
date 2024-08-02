b18d0957a4d14c89c5dbda51067660cc02881590
JosueGraao <miguelarno@protonmail.com>
2024-08-02 19:29:12 -0300

    script sql pets

---
+++ dbPets.sql
@@ -0,0 +1,9 @@
+-- apagar banco de dados
+drop database dbPets;
+-- criar banco de dados
+create database dbPets;
+-- acessar banco de dados
+use dbPets;
+-- visualizando banco de dados
+show databases;
+
