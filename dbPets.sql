d13724a2b8c322da52dd42b0dbc098441d32f26e
JosueGraao <miguelarno@protonmail.com>
2024-08-02 19:49:34 -0300

    script

--- dbPets.sql
+++ dbPets.sql
@@ -6,4 +6,20 @@ create database dbPets;
 use dbPets;
 -- visualizando banco de dados
 show databases;
-
+-- criando tabelas no banco de dados
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nomeUsu varchar(50) not null,
+senhaUsu varchar(10) not null,
+primary key(codUsu)
+);
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nomeFunc varchar(100) not null,
+emailFunc varchar(100),
+telCelFunc char(10),
+);
+-- visualizando tabelas
+show tables;
+-- visualizando a estrutura das tabelas
+desc tbUsuarios;
